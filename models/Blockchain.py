ff0bc1e6d6d1dfeadda64ce83c1e3964425d5f58
maneeshpm <manu.pm55@gmail.com>
2020-06-11 11:59:22 +0530

    :TODO: added blockchain model

---
+++ models/Blockchain.py
@@ -0,0 +1,22 @@
+from models import Block, Txn
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.unusedOP = {}
+        self.pendingTxn = []
+        self.currentTarget = None
+
+    def newBlockchain(self):
+        #return genesis block
+        pass
+
+    def add_block(self, block):
+        self.chain.append(block)
+
+    def isTxnValid(self, txn):
+        return txn.verifyTxn()
+
+    def isBlockValid(self, block):
+        pass
